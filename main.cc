a15153caf07c75e0991bd6b508140837dcc05ff5
Robin Lee <rgl@google.com>
2018-04-06 15:16:26 +0200

    Send everything through one adb shell subprocess

--- main.cc
+++ main.cc
@@ -1,13 +1,37 @@
+#include <cstdio>
 #include <map>
 #include <ncurses.h>
 #include <signal.h>
 #include <sstream>
 #include <string>
+#include <unistd.h>
 #include <vector>
 using namespace std;
 
 constexpr int ESC_KEY = 'e';
 constexpr int TEXT_KEY = 't';
+
+static FILE *adb_pipe_file = nullptr;
+
+FILE *adb_pipe() {
+    static char newline[2] = "\n";
+
+    if (adb_pipe_file != nullptr && write(fileno(adb_pipe_file), newline, 1) == EPIPE){
+        adb_pipe_file = nullptr;
+    }
+    if (adb_pipe_file == nullptr){
+        printw("Starting adb...\n");
+        adb_pipe_file = popen("/usr/local/google/home/rgl/bin/adb shell", "w");
+        setvbuf(adb_pipe_file, nullptr, _IONBF, 0);
+    }
+    return adb_pipe_file;
+}
+
+void close_adb_pipe(){
+    if (adb_pipe_file != nullptr) {
+        pclose(adb_pipe_file);
+    }
+}
 
 static int send_command(vector<char const*> const parts){
     stringstream sst;
@@ -19,12 +43,14 @@ static int send_command(vector<char const*> const parts){
     }
     string s=sst.str();
     printw("%s\n", s.c_str());
-    return system((s+=" &").c_str());
+    int res = fputs((s+="&").c_str(), adb_pipe());
+    fflush(adb_pipe());
+    return res;
 }
 
 // Returns exit code from system()
 static int send_keys(string const text){
-    return send_command({"adb","shell","input","text",text.c_str()});
+    return send_command({"input","text",text.c_str()});
 }
 
 // Returns exit code from system()
@@ -47,13 +73,14 @@ static int send_keys(int keycode){
         return -1;
     }
     string text=string("KEYCODE_") + key_mappings.at(keycode);
-    return send_command({"adb","shell","input","keyevent",text.c_str()});
+    return send_command({"input","keyevent",text.c_str()});
 }
 
 void usage(){
     printw("Press %c to exit, %c to send text\n", ESC_KEY, TEXT_KEY);
 }
 
+#include <iostream>
 int main(){
     initscr();
     raw();
@@ -61,6 +88,7 @@ int main(){
     noecho();
 
     halfdelay(2 /*=200ms*/);
+    adb_pipe();
 
     static volatile bool exit_handler_called=false;
     signal(SIGINT, [](int unused){exit_handler_called=true;});
@@ -86,12 +114,13 @@ int main(){
             }
             printw("\n");
             if (ch==0x0A){
-                send_keys("\\\"" + text.str() + "\\\"");
+                send_keys("\"" + text.str() + "\"");
             }
         }else{
-            send_keys(ch);
+            printw("Return code: %d\n", send_keys(ch));
         }
     }
 
     endwin();
+    close_adb_pipe();
 }
